902c8ffc26864d0719866ef2d68ba564056ce262
seandcook <sean.cook@ufl.edu>
2026-09-15 13:47:22 -0400

    Fixing some typos

--- backend/models/schemas.py
+++ backend/models/schemas.py
@@ -43,7 +43,7 @@ class AmountPayload(BaseModel):
 
 class BalanceResponse(BaseModel):
     accountId: int
-    balance: float
+    balance: Decimal
 
 class TransactionResponse(BaseModel):
     type: str
@@ -59,5 +59,5 @@ class TransferResponse(BaseModel):
     message: str
     sourceAccountId: int
     sourceBalance: Decimal
-    targetAccoundId: int
-
+    targetAccountId: int
+    targetBalance: Decimal
