ecfaf7269a8500327563db8188d5506e1ede41f7
Alexander Melendez <60493143+Alexander-Melendez@users.noreply.github.com>
2026-09-15 10:42:40 -0400

    Spaced things out for more readability and removed redundant comment

--- backend/models/account_entity.py
+++ backend/models/account_entity.py
@@ -14,17 +14,21 @@ class Account:
 
 # Deposits money to the account
   def deposit(self, amount: Decimal) -> Decimal:
+
     # Decimal value that must be positive according to business rules
     if amount <= 0:
       raise ValueError("Deposit amount must be positive")
+    
     self.balance += amount
     return self.balance
 
 # Withdraws money from the account
   def withdraw(self, amount: Decimal) -> Decimal:
+
     # Prevent negative withdrawals (giving yourself money)
     if amount <= 0:
       raise ValueError("Withdrawal amount must be positive")
+    
     # Cannot withdraw more than current balance according to business rules
     if amount > self.balance:
       raise ValueError("Cannot withdraw more than balance")
@@ -42,8 +46,7 @@ class SavingsAccount(Account):
 
 
 class CheckingAccount(Account):
-  """Child Class: Implements overdraft capability."""
-
   def __init__(self, account_id: int, user_id: int, balance: Decimal):
+    
     # Super init calls init constructor from the parent class so it can initalize with the given values
     super().__init__(account_id, user_id, "CHECKING", balance)
